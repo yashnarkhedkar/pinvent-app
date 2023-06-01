b1fd42b93a60a4786a6b3a0ac3006027af366843
yashnarkhedkar <yashnarkhedkar@gmail.com>
2023-06-01 22:07:35 +0530

    added frontent url in cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app", "http://localhost:8080"],
+    origin: ["http://localhost:3000", "https://inventeron.netlify.app", "http://localhost:8080"],
     credentials: true,
   })
 );
